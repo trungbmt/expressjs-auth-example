573eb91db08020fbd61a4296d35103d6d6ce0ba9
trungbmt <phamductrungbmt@gmail.com>
2024-05-12 10:18:00 +0700

    fix error handle

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
-var cors = require('cors')
+var cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./src/routes/auth.route");
@@ -13,12 +13,11 @@ const userRoutes = require("./src/routes/user.route");
 var app = express();
 
 app.use(logger("dev"));
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
@@ -40,8 +39,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({ message: err?.message });
 });
 
 module.exports = app;
